9ef4e0ff075c52ea10be2410697519f1a5a3d92b
Laurent Turek <laurent.turek@gmail.com>
2020-10-17 23:36:00 +0200

    #3 [ECOSYSTEM CHALLENGE] Using Remix And MetaMask On Moonbeam

---
+++ moonbeam/checkContract.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.0;
+
+contract checkContract {
+
+    address public address1 = 0x51eCa0433cd58426a23F14F7F9186d42CAcC4Ab5;
+    address public address2 = 0x87A4DE13910783F301F41d6BaC187df43A771244;
+    address public addressToken = 0x02d61353d10AD68ba08488E7123eAEa34DD0EB51;
+
+}
