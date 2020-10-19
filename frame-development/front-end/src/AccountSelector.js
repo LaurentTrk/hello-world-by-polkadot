1fc6ea07fb939e070f6d9722b34a31d154ce852c
Laurent Turek <laurent.turek@gmail.com>
2020-10-19 14:49:56 +0200

    Display KDT balance in the account selector (TODO : update the value when changed)

--- frame-development/front-end/src/AccountSelector.js
+++ frame-development/front-end/src/AccountSelector.js
@@ -1,132 +1,133 @@
-import React, { useState, useEffect } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import React, {useState, useEffect} from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 import {
-  Menu,
-  Button,
-  Dropdown,
-  Container,
-  Icon,
-  Image,
-  Label
+    Menu,
+    Button,
+    Dropdown,
+    Container,
+    Icon,
+    Image,
+    Label
 } from 'semantic-ui-react';
 
-import { useSubstrate } from './substrate-lib';
+import {useSubstrate} from './substrate-lib';
+import KdtContract, {defaultGasLimit, displayKdt} from "./KdtContract";
 
-function Main (props) {
-  const { keyring } = useSubstrate();
-  const { setAccountAddress } = props;
-  const [accountSelected, setAccountSelected] = useState('');
+function Main(props) {
+    const {keyring} = useSubstrate();
+    const {setAccountAddress} = props;
+    const [accountSelected, setAccountSelected] = useState('');
 
-  // Get the list of accounts we possess the private key for
-  const keyringOptions = keyring.getPairs().map(account => ({
-    key: account.address,
-    value: account.address,
-    text: account.meta.name.toUpperCase(),
-    icon: 'user'
-  }));
+    // Get the list of accounts we possess the private key for
+    const keyringOptions = keyring.getPairs().map(account => ({
+        key: account.address,
+        value: account.address,
+        text: account.meta.name.toUpperCase(),
+        icon: 'user'
+    }));
 
-  const initialAddress =
-    keyringOptions.length > 0 ? keyringOptions[0].value : '';
+    const initialAddress =
+        keyringOptions.length > 0 ? keyringOptions[0].value : '';
 
-  // Set the initial address
-  useEffect(() => {
-    setAccountAddress(initialAddress);
-    setAccountSelected(initialAddress);
-  }, [setAccountAddress, initialAddress]);
+    // Set the initial address
+    useEffect(() => {
+        setAccountAddress(initialAddress);
+        setAccountSelected(initialAddress);
+    }, [setAccountAddress, initialAddress]);
 
-  const onChange = address => {
-    // Update state with new account address
-    setAccountAddress(address);
-    setAccountSelected(address);
-  };
+    const onChange = address => {
+        // Update state with new account address
+        setAccountAddress(address);
+        setAccountSelected(address);
+    };
 
-  return (
-    <Menu
-      attached='top'
-      tabular
-      style={{
-        backgroundColor: '#fff',
-        borderColor: '#fff',
-        paddingTop: '1em',
-        paddingBottom: '1em'
-      }}
-    >
-      <Container>
-        <Menu.Menu>
-          <Image src={`${process.env.PUBLIC_URL}/assets/substrate-logo.png`} size='mini' />
-        </Menu.Menu>
-        <Menu.Menu position='right' style={{ alignItems: 'center' }}>
-          { !accountSelected
-            ? <span>
-              Add your account with the{' '}
-              <a
-                target='_blank'
-                rel='noopener noreferrer'
-                href='https://github.com/polkadot-js/extension'
-              >
-                Polkadot JS Extension
-              </a>
-            </span>
-            : null }
-          <CopyToClipboard text={accountSelected}>
-            <Button
-              basic
-              circular
-              size='large'
-              icon='user'
-              color={accountSelected ? 'green' : 'red'}
-            />
-          </CopyToClipboard>
-          <Dropdown
-            search
-            selection
-            clearable
-            placeholder='Select an account'
-            options={keyringOptions}
-            onChange={(_, dropdown) => {
-              onChange(dropdown.value);
+    return (
+        <Menu
+            attached='top'
+            tabular
+            style={{
+                backgroundColor: '#fff',
+                borderColor: '#fff',
+                paddingTop: '1em',
+                paddingBottom: '1em'
             }}
-            value={accountSelected}
-          />
-          <BalanceAnnotation accountSelected={accountSelected} />
-        </Menu.Menu>
-      </Container>
-    </Menu>
-  );
+        >
+            <Container>
+                <Menu.Menu>
+                    <Image src={`${process.env.PUBLIC_URL}/assets/substrate-logo.png`} size='mini'/>
+                </Menu.Menu>
+                <Menu.Menu position='right' style={{alignItems: 'center'}}>
+                    {!accountSelected
+                        ? <span>
+                            Add your account with the{' '}
+                            <a
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                href='https://github.com/polkadot-js/extension'
+                            >
+                                Polkadot JS Extension
+                            </a>
+                        </span>
+                        : null}
+                    <CopyToClipboard text={accountSelected}>
+                        <Button
+                            basic
+                            circular
+                            size='large'
+                            icon='user'
+                            color={accountSelected ? 'green' : 'red'}
+                        />
+                    </CopyToClipboard>
+                    <Dropdown
+                        search
+                        selection
+                        clearable
+                        placeholder='Select an account'
+                        options={keyringOptions}
+                        onChange={(_, dropdown) => {
+                            onChange(dropdown.value);
+                        }}
+                        value={accountSelected}
+                    />
+                    <BalanceAnnotation accountSelected={accountSelected}/>
+                </Menu.Menu>
+            </Container>
+        </Menu>
+    );
 }
 
-function BalanceAnnotation (props) {
-  const { accountSelected } = props;
-  const { api } = useSubstrate();
-  const [accountBalance, setAccountBalance] = useState(0);
+function BalanceAnnotation(props) {
+    const {accountSelected} = props;
+    const {api} = useSubstrate();
+    const [accountBalance, setAccountBalance] = useState(0);
+    const kdtContract = KdtContract(api);
 
-  // When account address changes, update subscriptions
-  useEffect(() => {
-    let unsubscribe;
+    // When account address changes, update subscriptions
+    useEffect(() => {
+        let unsubscribe;
 
-    // If the user has selected an address, create a new subscription
-    accountSelected &&
-      api.query.system.account(accountSelected, balance => {
-        setAccountBalance(balance.data.free.toHuman());
-      })
-        .then(unsub => {
-          unsubscribe = unsub;
-        })
-        .catch(console.error);
+        // If the user has selected an address, create a new subscription
+        accountSelected &&
 
-    return () => unsubscribe && unsubscribe();
-  }, [api, accountSelected]);
+        kdtContract.query.balanceOf(accountSelected, 0, defaultGasLimit, accountSelected).then(result => {
+            setAccountBalance(result.output.toHuman());
+        }).then(unsub => {
+            unsubscribe = unsub;
+        }).catch(console.error);
 
-  return accountSelected ? (
-    <Label pointing='left'>
-      <Icon name='money' color='green' />
-      {accountBalance}
-    </Label>
-  ) : null;
+        return () => unsubscribe && unsubscribe();
+    }, [api, accountSelected]);
+
+    return accountSelected ? (
+        <Label pointing='left'>
+            <Icon name='money' color='green'/>
+            {displayKdt(accountBalance)}
+        </Label>
+    ) : null;
 }
 
-export default function AccountSelector (props) {
-  const { api, keyring } = useSubstrate();
-  return keyring.getPairs && api.query ? <Main {...props} /> : null;
+export default function AccountSelector(props) {
+    const {api, keyring} = useSubstrate();
+    return keyring.getPairs && api.query ? <Main {...props} /> : null;
 }
