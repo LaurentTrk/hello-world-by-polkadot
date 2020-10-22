f8262867741edc7c89b298aad44dc3e1c2c03bba
Laurent Turek <laurent.turek@gmail.com>
2020-10-22 22:03:52 +0200

    Add fakesign function

---
+++ frame-development/front-end/src/substrate-lib/FakeSign.js
@@ -0,0 +1,27 @@
+import {useSubstrate} from "./SubstrateContext";
+
+export default async function fakeSign(api, transaction, account){
+    const signingOptions = await getSigningOptions(api, account);
+    return transaction.signFake(account, signingOptions);
+}
+
+const getSigningOptions = async (api, account) => {
+    return api.derive.tx.signingInfo(account).then((signingInfo)=> {
+        return buildSigningOptions(api, signingInfo);
+    });
+}
+
+const buildSigningOptions = (api, signingInfo) => {
+    return {
+        genesisHash: api.genesisHash,
+        runtimeVersion: api.runtimeVersion,
+        signedExtensions: api.registry.signedExtensions,
+        version: api.extrinsicType,
+        blockHash: signingInfo.header.hash,
+        era: api.registry.createType('ExtrinsicEra', {
+            current: signingInfo.header.number,
+            period: signingInfo.mortalLength
+        }),
+        nonce: signingInfo.nonce
+    }
+}
